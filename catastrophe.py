d534688432f2d42319b69927c4e09bac7a912050
smccarthy0426 <smccarthy@csumb.edu>
2015-03-04 15:03:20 -0800

    gameLoop
    created a game loop

--- catastrophe.py
+++ catastrophe.py
@@ -15,37 +15,40 @@ pygame.display.set_caption('Catatstrophe')
 myImage = pygame.image.load("nyan3.gif")
 imagerect = myImage.get_rect()
 
-gameEnd = False
 
 
-lead_x = 750
-lead_y = randint(0,450)
-lead_x_change = 10
-cat_x = 10
-cat_y = 150
+
+
 
 clock = pygame.time.Clock()
-while not gameEnd:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+def gameLoop():
+    gameEnd = False
+    lead_x = 750
+    lead_y = randint(0,450)
+    lead_x_change = 10
+    cat_x = 10
+    cat_y = 150
+    while not gameEnd:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameEnd = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    cat_y += 50
+                if event.key == pygame.K_UP:
+                    cat_y -= 50
+        if cat_y >= 450 or cat_y <= 0:
             gameEnd = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                cat_y += 50
-            if event.key == pygame.K_UP:
-                cat_y -= 50
-    if cat_y > 450:
-        gameEnd = True
-    elif cat_y < 0:
-        gameEnd = True
-    lead_x -= lead_x_change
+        lead_x -= lead_x_change
 
-    gameDisplay.fill(white)
+        gameDisplay.fill(white)
 
-    pygame.draw.rect(gameDisplay, pink, [lead_x,lead_y,20,20])
-    gameDisplay.blit(myImage,(cat_x, cat_y))
-    pygame.display.update()
-    
-    clock.tick(30)
-pygame.quit()
-quit()
+        enemy = pygame.draw.rect(gameDisplay, pink, [lead_x,lead_y,20,20])
+        gameDisplay.blit(myImage,(cat_x, cat_y))
+        pygame.display.update()
+        
+        clock.tick(30)
+    pygame.quit()
+    quit()
+
+gameLoop()
