9636b62ea85e7a24c2d83fd7c43fa58d8e20b93f
smccarthy0426 <smccarthy@csumb.edu>
2015-03-04 15:39:30 -0800

    End of 3/4/2015 work
    Updated variables to make it easier to randomize enemies.

--- catastrophe.py
+++ catastrophe.py
@@ -6,7 +6,11 @@ pygame.init()
 
 white = (255,255,255)
 pink = (205, 140, 149)
-gameDisplay = pygame.display.set_mode((800, 450))
+black = (0,0,0)
+
+display_w = 800
+display_h = 450
+gameDisplay = pygame.display.set_mode((display_w,display_h))
 
 pygame.display.update()
 pygame.display.set_caption('Catatstrophe')
@@ -15,20 +19,34 @@ pygame.display.set_caption('Catatstrophe')
 myImage = pygame.image.load("nyan3.gif")
 imagerect = myImage.get_rect()
 
-
-
+font = pygame.font.SysFont(None, 25)
+def message_to_screen(msg,color):
+    screen_text = font.render(msg, True, color)
+    gameDisplay.blit(screen_text, [250, 250])
 
 
 
 clock = pygame.time.Clock()
 def gameLoop():
     gameEnd = False
-    lead_x = 750
-    lead_y = randint(0,450)
-    lead_x_change = 10
+    block_size = 15
+    enemy_x = display_w - block_size
+    enemy_y = randint(0,display_h)
+    enemy_x_change = 10
     cat_x = 10
     cat_y = 150
+    gameOver = False
     while not gameEnd:
+
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("You failed. Press C to continue", black)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_c:
+                        gameLoop()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameEnd = True
@@ -38,12 +56,12 @@ def gameLoop():
                 if event.key == pygame.K_UP:
                     cat_y -= 50
         if cat_y >= 450 or cat_y <= 0:
-            gameEnd = True
-        lead_x -= lead_x_change
+            gameOver = True
+        enemy_x -= enemy_x_change
 
         gameDisplay.fill(white)
 
-        enemy = pygame.draw.rect(gameDisplay, pink, [lead_x,lead_y,20,20])
+        enemy = pygame.draw.rect(gameDisplay, pink, [enemy_x,enemy_y,block_size,block_size])
         gameDisplay.blit(myImage,(cat_x, cat_y))
         pygame.display.update()
         
